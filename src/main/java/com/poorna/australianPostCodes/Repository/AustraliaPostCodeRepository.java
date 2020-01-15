791843d0658cb02f1cebeca101118c94483b21ec
Poorna Jayasinghe <poornasjayasinghe@gmail.com>
2020-01-15 11:08:33 +1100

    Finalize postcode data cleaning

--- src/main/java/com/poorna/australianPostCodes/Repository/AustraliaPostCodeRepository.java
+++ src/main/java/com/poorna/australianPostCodes/Repository/AustraliaPostCodeRepository.java
@@ -3,5 +3,5 @@ package com.poorna.australianPostCodes.Repository;
 import com.poorna.australianPostCodes.Entity.AustralianPostCode;
 import org.springframework.data.repository.CrudRepository;
 
-public interface AustraliaPostCodeRepository extends CrudRepository<AustralianPostCode, Integer> {
+public interface AustraliaPostCodeRepository extends CrudRepository<AustralianPostCode, Long> {
 }
